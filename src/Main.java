0b8258558564194da51877fb5d1c19cd7440a25d
mxx567 <themaks567@gmail.com>
2026-09-11 20:32:43 +0500

    Main method with tests have been created

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import classes.*;
+
 void main() {
+    Director dir = new Director();
+
+    Videogame racingGame = dir.createMultiplayerRacingGame("MP Racing");
+    Videogame shooterGame = dir.createConsoleMultiplayerShooterGame("shootergame");
+
+    System.out.println(racingGame);
+    System.out.println(shooterGame);
 }
